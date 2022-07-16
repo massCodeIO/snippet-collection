7ebbde477de999c79a4a7ca743fba3ac6ea922a4
Anton Reshetov <reshetov.art@gmail.com>
2022-07-16 12:12:40 +0300

    build: add styles

--- nuxt.config.js
+++ nuxt.config.js
@@ -18,7 +18,12 @@ export default {
   },
 
   // Global CSS: https://go.nuxtjs.dev/config-css
-  css: [],
+  css: [
+    {
+      src: '~assets/scss/main.scss',
+      lang: 'scss'
+    }
+  ],
 
   // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
   plugins: ['~plugins/read.js'],
