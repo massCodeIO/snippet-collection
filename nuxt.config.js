4035758dcbbd5ec09d6141c3cc3a910cb7edd655
Anton Reshetov <reshetov.art@gmail.com>
2022-07-17 11:09:37 +0300

    chore: clean

--- nuxt.config.js
+++ nuxt.config.js
@@ -28,7 +28,7 @@ export default {
   ],
 
   // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
-  plugins: ['~plugins/read.js'],
+  plugins: [],
 
   // Auto import components: https://go.nuxtjs.dev/config-components
   components: true,
