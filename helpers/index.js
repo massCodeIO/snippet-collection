82f3d561417ae4a5a83a720dfd4f9cf511ad4510
Anton Reshetov <reshetov.art@gmail.com>
2022-07-18 06:05:22 +0300

    feat(helpers): add helpers

---
+++ helpers/index.js
@@ -0,0 +1,22 @@
+export const getCodeByLang = (lang = 'html', string) => {
+  const htmlRe = /```html\n([\S\s]+?)\n```/
+  const cssRe = /```css\n([\S\s]+?)\n```/
+
+  return string.match(lang === 'html' ? htmlRe : cssRe)[1]
+}
+
+export const formatSnippetData = (data) => {
+  return {
+    id: data.slug,
+    title: data.title,
+    css: getCodeByLang('css', data.text),
+    html: getCodeByLang('html', data.text),
+    author: data.author,
+    date: data.createdAt,
+    category: data.path.split('/')[1]
+  }
+}
+
+export const capFirst = (string) => {
+  return string.charAt(0).toUpperCase() + string.slice(1)
+}
