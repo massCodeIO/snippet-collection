f5e657f3acb24b7ad6c82486f9a7cf20ded6fb76
Anton Reshetov <reshetov.art@gmail.com>
2022-07-18 08:07:29 +0300

    feat(pages): add index page

---
+++ pages/index.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="index-page">
+    <SnippetList :list="snippetsByCategory" />
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'IndexPage',
+
+  asyncData ({ store }) {
+    return {
+      snippets: store.state.snippets
+    }
+  },
+
+  computed: {
+    snippetsByCategory () {
+      return !this.$route.query.category
+        ? this.snippets
+        : this.snippets.filter((i) => i.category === this.$route.query.category)
+    }
+  }
+}
+</script>
