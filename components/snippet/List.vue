63472b8ca1b966314046e384bae32213e4636ae6
Anton Reshetov <reshetov.art@gmail.com>
2022-07-18 08:08:19 +0300

    feat: add snippet list

---
+++ components/snippet/List.vue
@@ -0,0 +1,38 @@
+<template>
+  <div class="snippet-list">
+    <SnippetCard
+      v-for="(i, index) in list"
+      :key="index"
+      :title="i.title"
+      :author="i.author"
+      :css="i.css"
+      :html="i.html"
+      :link="`snippet/${i.category}/${i.id}`"
+    />
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'SnippetList',
+
+  props: {
+    list: {
+      type: Array,
+      default: () => []
+    }
+  },
+
+  data () {
+    return {}
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+.snippet-list {
+  display: grid;
+  grid-template-columns: repeat(auto-fill, minmax(250px, 1fr));
+  gap: var(--spacing-md);
+}
+</style>
