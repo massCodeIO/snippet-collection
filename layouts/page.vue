80b38ed715fd846396621f37177c8620c019b847
Anton Reshetov <reshetov.art@gmail.com>
2022-07-18 08:44:53 +0300

    feat(pages): add snippet page

---
+++ layouts/page.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="editor-layout">
+    <TheHeader />
+    <div class="container">
+      <div class="main">
+        <div class="content">
+          <Nuxt />
+        </div>
+      </div>
+      <TheFooter />
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'PageLayout',
+
+  data () {
+    return {}
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+.main {
+  padding-top: var(--header-height);
+}
+</style>
